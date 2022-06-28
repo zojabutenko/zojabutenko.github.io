c7ed7ff75b4c736d4be2e27c36b862cb042d911e
zojabutenko <zoja.butenko@yandex.ru>
2022-06-28 04:26:27 +0200

    upd

---
+++ README.md
@@ -0,0 +1,4 @@
+# construxercise-rus
+Hands-on learning of Russian constructions
+
+site available at https://zojabutenko.github.io/index.html
---
+++ js/test.js
@@ -0,0 +1,16 @@
+// $('.collapse').collapse()
+
+// $(document).on('.data-api')
+
+
+var coll = document.getElementById("accordion");
+let children = coll.children;
+
+
+
+// for (i = 0; i < children.length; i++) {
+//     alert(children[i])
+
+// }
+
+console.log(children);
