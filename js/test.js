0d5a512d5a7e75b07b187bb948ecfe12d8c572e9
miltend <miltendean@gmail.com>
2022-07-08 22:16:25 +0200

    add_table_auto

--- js/test.js
+++ js/test.js
@@ -2,20 +2,38 @@
 
 const tree = document.createDocumentFragment();
 
-let r = await axios.get("https://raw.githubusercontent.com/zojabutenko/zojabutenko.github.io/main/test.yml");
-let json_data = jsyaml.load(r.data);
+let r = await axios.get("https://raw.githubusercontent.com/zojabutenko/zojabutenko.github.io/main/data/lessons.yml");
+let json_data = jsyaml.loadAll(r.data);
 let records = {};
+
 
 for (let key of Object.keys(json_data)) {
     records[key] = json_data[key];
 }
 
 
+// берет номер урока с title'а страницы (пока -2, тк тестовый ямл, исправить потом)
+const lesson_id = document.title.split(" ")[1] - 2
+
+const data_constructions = records[0][lesson_id]
+
+
+
 const part_1 = document.createElement("div")
 part_1.setAttribute("class", "container px-4")
 
-const title = document.createElement("h1")
-title.appendChild(document.createTextNode(`Урок ${records["lesson_id"]}`));
+const lesson_title = document.createElement("h1")
+lesson_title.appendChild(document.createTextNode(data_constructions["lesson_name"]));
+
+// file link
+const file = document.createElement("a")
+file.setAttribute("class", "btn btn-light float-end")
+file.setAttribute("href", `../../files/Lesson%20${data_constructions["lesson_id"]}.%20Download.pdf`)
+file.setAttribute("role", "button")
+file.setAttribute("target", "_blank")
+file.appendChild(document.createTextNode("Download"))
+lesson_title.appendChild(file)
+
 
 const constructions = document.createElement("h3")
 constructions.appendChild(document.createTextNode("Конструкции урока"));
@@ -23,14 +41,60 @@ constructions.appendChild(document.createTextNode("Конструкции уро
 const constr_table = document.createElement("table")
 constr_table.setAttribute("class", "table")
 
-// const 
+const thead = document.createElement("thead")
+thead.setAttribute("class", "table-primary")
+
+const tr_head = document.createElement("tr")
 
 
-// table
+for (var i = 0; i <=2; i++) {
+    var t = document.createElement("th")
+    t.setAttribute("scope", "col")
+    t.appendChild(document.createTextNode(data_constructions["construction_table_header"][i]))
+    tr_head.appendChild(t)
+}
 
-part_1.appendChild(title)
+constr_table.appendChild(thead)
+thead.appendChild(tr_head)
+
+const tbody = document.createElement("tbody")
+constr_table.appendChild(tbody)
+
+// console.log(data_constructions["construction_table_rows"].length)
+
+for (var i = 0; i < data_constructions["construction_table_rows"].length; i++) {
+    var tr = document.createElement("tr")
+    
+    for (var k = 0; k <=2; k++) {
+        if (k == 0) {
+            var t = document.createElement("th")
+            t.setAttribute("scope", "row")
+        } else {
+            var t = document.createElement("td")
+        }
+        t.appendChild(document.createTextNode(data_constructions["construction_table_rows"][i][k]))
+        tr.appendChild(t)
+    }
+    tbody.appendChild(tr)
+}
+
+
+const abbvs = document.createElement("p")
+
+for (var i = 0; i < data_constructions["lesson_instructions"].length; i++) {
+    abbvs.appendChild(document.createTextNode(data_constructions["lesson_instructions"][i]))
+    abbvs.appendChild(document.createElement("br"))
+}
+
+
+
+
+
+part_1.appendChild(lesson_title)
 part_1.appendChild(constructions)
 part_1.appendChild(constr_table)
+part_1.appendChild(abbvs)
+
 
 
 
